40ce2c9a7fbb73f50d3010c10d1e39e9357f0e4b
Gabriel Vieira Andrian <gabriel_andrian@hotmail.com>
2021-04-10 23:50:44 -0300

    feat: Empty users

--- src/components/users-map/index.jsx
+++ src/components/users-map/index.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { useUsers } from "../../providers/users";
 import Card from "../Card";
+import { Empty } from "antd";
 
 const UsersMap = () => {
   const { users } = useUsers();
 
+  if (users.length === 0) {
+    return <Empty />;
+  }
   return (
     <div>
       {users.map((user) => {
