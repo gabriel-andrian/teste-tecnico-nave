c3931410babcf91f1cb035e882f60824eadba8bc
Gabriel Vieira Andrian <gabriel_andrian@hotmail.com>
2021-04-11 00:00:38 -0300

    fix: Loading icon

--- src/components/users/index.jsx
+++ src/components/users/index.jsx
@@ -1,11 +1,24 @@
 import React from "react";
 import { useUsers } from "../../providers/users";
 import UsersMap from "../users-map";
+import { LoadingOutlined } from "@ant-design/icons";
+import { Spin, Alert } from "antd";
+import { LoadingContainer } from "./style";
 
 const Users = () => {
   const { loading } = useUsers();
 
-  return <>{loading ? <h1>LOADING...</h1> : <UsersMap />}</>;
+  return (
+    <>
+      {loading ? (
+        <LoadingContainer>
+          <Spin tip="Loading..." size="large" />
+        </LoadingContainer>
+      ) : (
+        <UsersMap />
+      )}
+    </>
+  );
 };
 
 export default Users;
